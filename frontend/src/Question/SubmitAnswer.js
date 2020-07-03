ec3f9bf6640801c629c31c3cdbfbb3918c91b0a1
Malasia Woods <malasiawoods@Malasias-MacBook-Air.local>
2020-07-03 01:42:49 -0700

    building submit file

---
+++ frontend/src/Question/SubmitAnswer.js
@@ -0,0 +1,11 @@
+import React, {Component, Fragment} from 'react';
+import {withRouter} from 'react-router-dom';
+import auth0Client from '../Auth';
+
+class SubmitAnswer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      answer: '',
+    };
+  }
