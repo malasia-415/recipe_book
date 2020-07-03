43bebaaef0597cdf1238fc92c614bf22d9ddb524
Malasia Woods <malasiawoods@Malasias-MacBook-Air.local>
2020-07-03 00:54:16 -0700

    setup Auth0 in index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
 
 // define the Express app
 const app = express();
@@ -40,6 +42,20 @@ app.get('/:id', (req, res) => {
   if (question.length > 1) return res.status(500).send();
   if (question.length === 0) return res.status(404).send();
   res.send(question[0]);
+});
+
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://dev-ete98t3j.us.auth0.com/.well-known/jwks.json`
+  }),
+
+  // Validate the audience and the issuer.
+  audience: '2nq2QW9fzFjXTk860g92gb4z7A4Z6IbJ',
+  issuer: `https://dev-ete98t3j.us.auth0.com/`,
+  algorithms: ['RS256']
 });
 
 // insert a new question
