6e2465a99a991d4a5236c96a8c7deedc47b75c41
Malasia Woods <malasiawoods@Malasias-MacBook-Air.local>
2020-07-02 22:39:34 -0700

    defin express enhance Helmet and parse content

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,3 +7,12 @@ const morgan = require('morgan');
 
 // define the Express app
 const app = express();
+
+// the database
+const questions = [];
+
+// enhance your app security with Helmet
+app.use(helmet());
+
+// use bodyParser to parse application/json content-type
+app.use(bodyParser.json());
