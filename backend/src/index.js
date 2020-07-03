69ca6c77eec8ea0a5ede7e406fd4112e4c1f8132
Malasia Woods <malasiawoods@Malasias-MacBook-Air.local>
2020-07-02 22:40:54 -0700

    enable cors recieve all questions

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,3 +16,20 @@ app.use(helmet());
 
 // use bodyParser to parse application/json content-type
 app.use(bodyParser.json());
+
+// enable all CORS requests
+app.use(cors());
+
+// log HTTP requests
+app.use(morgan('combined'));
+
+// retrieve all questions
+app.get('/', (req, res) => {
+  const qs = questions.map(q => ({
+    id: q.id,
+    title: q.title,
+    description: q.description,
+    answers: q.answers.length,
+  }));
+  res.send(qs);
+});
