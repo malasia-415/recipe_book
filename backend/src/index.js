a1f4b45cf45d29cfde85682cf1953831a1313aac
Malasia Woods <malasiawoods@Malasias-MacBook-Air.local>
2020-07-02 22:37:21 -0700

    Building index.js

---
+++ backend/src/index.js
@@ -0,0 +1,9 @@
+//import dependencies
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+
+// define the Express app
+const app = express();
