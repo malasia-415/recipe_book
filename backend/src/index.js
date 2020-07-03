9cdc2daac008b45e977fb25f9c56748393720dfd
Malasia Woods <malasiawoods@Malasias-MacBook-Air.local>
2020-07-02 23:50:04 -0700

    modified backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -36,41 +36,41 @@ app.get('/', (req, res) => {
 
 // get a specific question
 app.get('/:id', (req, res) => {
-    const question = questions.filter(q => (q.id === parseInt(req.params.id)));
-    if (question.length > 1) return res.status(500).send();
-    if (question.length === 0) return res.status(404).send();
-    res.send(question[0]);
-  });
-  
-  // insert a new question
-  app.post('/', (req, res) => {
-    const {title, description} = req.body;
-    const newQuestion = {
-      id: questions.length + 1,
-      title,
-      description,
-      answers: [],
-    };
-    questions.push(newQuestion);
-    res.status(200).send();
+  const question = questions.filter(q => (q.id === parseInt(req.params.id)));
+  if (question.length > 1) return res.status(500).send();
+  if (question.length === 0) return res.status(404).send();
+  res.send(question[0]);
+});
+
+// insert a new question
+app.post('/', (req, res) => {
+  const {title, description} = req.body;
+  const newQuestion = {
+    id: questions.length + 1,
+    title,
+    description,
+    answers: [],
+  };
+  questions.push(newQuestion);
+  res.status(200).send();
+});
+
+// insert a new answer to a question
+app.post('/answer/:id', (req, res) => {
+  const {answer} = req.body;
+
+  const question = questions.filter(q => (q.id === parseInt(req.params.id)));
+  if (question.length > 1) return res.status(500).send();
+  if (question.length === 0) return res.status(404).send();
+
+  question[0].answers.push({
+    answer,
   });
 
-  // insert a new answer to a question
-app.post('/answer/:id', (req, res) => {
-    const {answer} = req.body;
-  
-    const question = questions.filter(q => (q.id === parseInt(req.params.id)));
-    if (question.length > 1) return res.status(500).send();
-    if (question.length === 0) return res.status(404).send();
-  
-    question[0].answers.push({
-      answer,
-    });
-  
-    res.status(200).send();
-  });
-  
-  // start the server
-  app.listen(8081, () => {
-    console.log('listening on port 8081');
-  });
+  res.status(200).send();
+});
+
+// start the server
+app.listen(8081, () => {
+  console.log('listening on port 8081');
+});
