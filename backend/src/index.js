d0d4bf8505c324b6c7567cde65b43bb6602c3564
Malasia Woods <malasiawoods@Malasias-MacBook-Air.local>
2020-07-02 22:43:33 -0700

    insert new and start server

--- backend/src/index.js
+++ backend/src/index.js
@@ -54,3 +54,23 @@ app.get('/:id', (req, res) => {
     questions.push(newQuestion);
     res.status(200).send();
   });
+
+  // insert a new answer to a question
+app.post('/answer/:id', (req, res) => {
+    const {answer} = req.body;
+  
+    const question = questions.filter(q => (q.id === parseInt(req.params.id)));
+    if (question.length > 1) return res.status(500).send();
+    if (question.length === 0) return res.status(404).send();
+  
+    question[0].answers.push({
+      answer,
+    });
+  
+    res.status(200).send();
+  });
+  
+  // start the server
+  app.listen(8081, () => {
+    console.log('listening on port 8081');
+  });
