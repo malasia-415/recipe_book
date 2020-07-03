1a23c2187aedd1cc7422d84a3d1512cbcbb5d508
Malasia Woods <malasiawoods@Malasias-MacBook-Air.local>
2020-07-03 01:25:13 -0700

    build render function

--- frontend/NewQuestion/NewQuestion.js
+++ frontend/NewQuestion/NewQuestion.js
@@ -40,3 +40,48 @@ class NewQuestion extends Component {
   
       this.props.history.push('/');
     }
+
+    render() {
+        return (
+          <div className="container">
+            <div className="row">
+              <div className="col-12">
+                <div className="card border-primary">
+                  <div className="card-header">New Question</div>
+                  <div className="card-body text-left">
+                    <div className="form-group">
+                      <label htmlFor="exampleInputEmail1">Title:</label>
+                      <input
+                        disabled={this.state.disabled}
+                        type="text"
+                        onBlur={(e) => {this.updateTitle(e.target.value)}}
+                        className="form-control"
+                        placeholder="Give your question a title."
+                      />
+                    </div>
+                    <div className="form-group">
+                      <label htmlFor="exampleInputEmail1">Description:</label>
+                      <input
+                        disabled={this.state.disabled}
+                        type="text"
+                        onBlur={(e) => {this.updateDescription(e.target.value)}}
+                        className="form-control"
+                        placeholder="Give more context to your question."
+                      />
+                    </div>
+                    <button
+                      disabled={this.state.disabled}
+                      className="btn btn-primary"
+                      onClick={() => {this.submit()}}>
+                      Submit
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        )
+      }
+    }
+    
+    export default withRouter(NewQuestion);
