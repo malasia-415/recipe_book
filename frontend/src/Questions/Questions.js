e57ab60baf491b6062198c351897c0a8d5b702a9
Malasia Woods <malasiawoods@Malasias-MacBook-Air.local>
2020-07-03 00:11:01 -0700

    building questions.js

---
+++ frontend/src/Questions/Questions.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+import axios from 'axios';
+
+class Questions extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      questions: null,
+    };
+  }
+
+  async componentDidMount() {
+    const questions = (await axios.get('http://localhost:8081/')).data;
+    this.setState({
+      questions,
+    });
+  }
