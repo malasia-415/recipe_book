d3eaf070a793b9f473c54a3190e77aae50fe34c8
Malasia Woods <malasiawoods@Malasias-MacBook-Air.local>
2020-07-03 01:04:27 -0700

    building auth.js

---
+++ frontend/src/Auth.js
@@ -0,0 +1,20 @@
+import auth0 from 'auth0-js';
+
+class Auth {
+  constructor() {
+    this.auth0 = new auth0.WebAuth({
+      // the following three lines MUST be updated
+      domain: '<YOUR_AUTH0_DOMAIN>',
+      audience: 'https://<YOUR_AUTH0_DOMAIN>/userinfo',
+      clientID: '<YOUR_AUTH0_CLIENT_ID>',
+      redirectUri: 'http://localhost:3000/callback',
+      responseType: 'id_token',
+      scope: 'openid profile'
+    });
+
+    this.getProfile = this.getProfile.bind(this);
+    this.handleAuthentication = this.handleAuthentication.bind(this);
+    this.isAuthenticated = this.isAuthenticated.bind(this);
+    this.signIn = this.signIn.bind(this);
+    this.signOut = this.signOut.bind(this);
+  }
